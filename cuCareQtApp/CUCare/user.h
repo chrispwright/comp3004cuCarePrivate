e20464e71eb89101a84590531e374e75300bdb11
Calvin Lewis <calvin.lewis@me.com>
2012-10-30 11:20:07 -0400

    played around with user
    made sure that my GitHub works, just did some quick edits to user.h to
    make sure it works

--- cuCareQtApp/CUCare/user.h
+++ cuCareQtApp/CUCare/user.h
@@ -2,14 +2,25 @@
 #define USER_H
 
 #include <QString>
+
 class User
 {
 public:
-    User();
+    User(); //default constructor
+    //User(QString, QString, QString, QString); //constructor that takes in type, id, first/last name parameters
+
+    QString getType(); //returns type of user
+    //QString getUserId(); //returns id of user
+    QString getFirstName(); //returns first name of user
+    QString getLastName(); //returns last name of user
+
+    void setType(QString); //sets type of user
+    void setFirstName(QString); //sets first name of user
+    void setLastName(QString); //sets last name of user
 
 private:
     QString *type;
-    QString *userId;
+    //QString *userId;
     QString *firstName;
     QString *lastName;
 };
